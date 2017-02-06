ffb75c77bb98ea08f49d05c9c896831c1f32ac2e
vuurball <oneindigedroom@gmail.com>
2017-02-06 16:44:17 +0200

    finished server side functionality

--- top.js
+++ top.js
@@ -3,32 +3,38 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var r = require("rethinkdb");
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/chat.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/top.html');
 });
 
-io.on('connection', function(socket){
+var query = r.db('test').table('players').orderBy({index: r.desc('score')}).limit(5);
 
-    io.emit('chat message', 'OLGAAAAAAAAAAAAAAAAA');
+//on updated
+r.connect().then(function (conn) {
+    return query.changes().run(conn);
+}).then(function (cursor) {
+    cursor.each(function (err, item) {
+        console.log(item);
+        io.emit('chat message', item);
+    });
+});
 
-
-r.connect().then(function(conn){
-                return r.db('test').table('players').orderBy({index: r.desc('score')}).limit(5).changes().run(conn);
-        })
-        .then(function(cursor){
-                cursor.each(function(err, item){
- io.emit('chat message', item);
-                        console.log(item);
-                });
+io.on('connection', function (socket) {
+    console.log('connected - initializing..');
+    r.connect().then(function (conn) {
+        return query.run(conn).finally(function () {
+            conn.close();
         });
-
-
-	console.log('a user connected');
-//	socket.on('chat message', function(msg){
-//		io.emit('chat message', msg);
-//	  });
+    }).then(function (cursor) {
+        return cursor.toArray();
+    }).then(function (output) {
+        console.log("Query output:", output);
+        io.emit('leaders', output);
+    }).error(function (err) {
+        console.log("Failed:", err);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
